f1e00eebf4b34d92a947bb3564b1ffc7a2ad5ed5
Tez-Tez-Tez <tezclaudia4@gmail.com>
2025-09-18 17:59:02 -0500

    Update UsuariosRoutes.js

--- src/main/routes/UsuariosRoutes.js
+++ src/main/routes/UsuariosRoutes.js
@@ -1,17 +1,9 @@
 const express = require('express')
 const db = require('../config/db.js')
+const UsuariosController = require('../controllers/UsuariosController.js')
 
 const router = express.Router();
 
-router.get('/usuarios',async(req,res)=>{
-    try {
-        const sql = 'SELECT * FROM usuarios;';
-        const [rows] = await db.query(sql);
-        res.json(rows)
-    } catch (error) {
-        console.error('Error al obtener usuarios',error);
-        res.status(500).json({error:'Error en el server'})
-    }
-})
+router.get('/',UsuariosController.obtenerTodos)
 
 module.exports = router;
