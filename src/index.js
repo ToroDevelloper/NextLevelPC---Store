268aebd7be8bc767e75d513b91f104bf9f5f40f9
Tez-Tez-Tez <tezclaudia4@gmail.com>
2025-09-18 17:05:59 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const db = require('./main/config/db.js');
+const usuariosRoutes = require('./main/routes/UsuariosRoutes.js')
 
 const app = express();
 app.use(express.json());
+
+app.use('/api',usuariosRoutes)
 
 const port = 8080;
 app.listen(port,()=>{
