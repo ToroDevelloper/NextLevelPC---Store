d5faa6b069f771781ce329d795e1981d2e16d311
Tez-Tez-Tez <tezclaudia4@gmail.com>
2025-09-17 21:54:59 -0500

    server en index

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const db = require('./main/db.js');
+
+const app = express();
+app.use(express.json());
+
+const port = 8080;
+app.listen(port,()=>{
+    console.log('server en port '+ port)
+})
+
+
