e0c966da4d24d6a868add92ce34dd7ec3e4fe32d
Tez-Tez-Tez <tezclaudia4@gmail.com>
2025-09-18 20:41:33 -0500

    Update UsuariosController.js

--- src/main/controllers/UsuariosController.js
+++ src/main/controllers/UsuariosController.js
@@ -1,3 +1,4 @@
+const db = require('../config/db.js');
 const Usuarios = require('../models/Usuarios.js')
 
 class UsuariosController{
@@ -8,6 +9,21 @@ class UsuariosController{
            res.json(data);
         } catch (error) {
            res.status(500).json({mensaje:'Error al obtener usuarios',error})
+        }
+    }
+
+    static async crear(req,res){
+        try{
+        const data = req.body;
+        const {Nombre,Apellido,Cedula,Celular,Correo,Password,Direccion} = data;
+        const email = await Usuarios.correo(Correo);
+        if(email){
+           return res.status(400).json({mensaje:'Correo en uso por otro usuario'})
+        }
+        const result = await Usuarios.crear(data);
+        res.status(201).json({mensaje:'Usuario creado exitosamente'})
+    }catch(error){
+          res.status(500).json({mensaje:'Error al crear el usuario'})
         }
     }
 }
