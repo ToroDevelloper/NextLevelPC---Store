082cb09c6bcba9b07b81073694332b1ef191a660
Tez-Tez-Tez <tezclaudia4@gmail.com>
2025-09-18 20:43:17 -0500

    Update UsuariosRoutes.js

--- src/main/routes/UsuariosRoutes.js
+++ src/main/routes/UsuariosRoutes.js
@@ -4,6 +4,8 @@ const UsuariosController = require('../controllers/UsuariosController.js')
 
 const router = express.Router();
 
-router.get('/',UsuariosController.obtenerTodos)
+router.get('/',UsuariosController.obtenerTodos);
+router.post('/',UsuariosController.crear);
 
 module.exports = router;
+
