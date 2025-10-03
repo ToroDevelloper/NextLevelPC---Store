ef97d6e97c67c90266ddfb6427a90a590acf2c85
andres2003xd <machoandres12@gmail.com>
2025-10-03 17:19:19 -0500

    Metodo que muestra todos los items existentes

--- backend/controllers/OrdenItemsController.js
+++ backend/controllers/OrdenItemsController.js
@@ -3,14 +3,25 @@ class OrdenItemsController {
         try {
             const data = req.body;
             const insertId = await ordenItemsService.crear(data);
-            
-            res.status(201).json({ 
-                mensaje: 'Item creado exitosamente', 
-                item_id: insertId 
+
+            res.status(201).json({
+                mensaje: 'Item creado exitosamente',
+                item_id: insertId
             });
         } catch (error) {
             console.error("Error en crear:", error);
             res.status(500).json({ mensaje: 'Error al crear el item: ' + error.message });
         }
     }
+
+    static async obtenerTodos(req, res) {
+        try {
+            const items = await ordenItemsService.obtenerTodos();
+            res.status(200).json(items);
+        } catch (error) {
+            console.error("Error en obtenerTodos:", error);
+            res.status(500).json({ mensaje: 'Error al obtener todos los items: ' + error.message });
+        }
+    }
+
 }
