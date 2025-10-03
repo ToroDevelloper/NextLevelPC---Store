ef5ac60ce9425837d78105bfa55d8f9ea6227258
andres2003xd <machoandres12@gmail.com>
2025-10-03 17:23:55 -0500

    Se define endpoints para gestionar items dentro de las órdenes

---
+++ backend/routes/OrdenItems.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const OrdenItemsController = require('../controllers/OrdenItemsController.js');
+
+const router = express.Router();
+
+router.get('/', OrdenItemsController.obtenerTodos);  // ← NUEVA RUTA
+router.post('/', OrdenItemsController.crear);
+router.put('/:id', OrdenItemsController.actualizar);
+router.delete('/:id', OrdenItemsController.eliminar);
+router.get('/orden/:ordenId', OrdenItemsController.obtenerPorOrden);
+
+module.exports = router;
