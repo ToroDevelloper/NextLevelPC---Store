24554971a628648ab13967832a084a43e51f6c5e
andres2003xd <machoandres12@gmail.com>
2025-10-03 17:23:05 -0500

    Se define los endpoints HTTP para el manejo de órdenes de ordenes

---
+++ backend/routes/Ordenes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const OrdenesController = require('../controllers/OrdenesController.js');
+
+const router = express.Router();
+
+router.get('/', OrdenesController.obtenerTodos);
+router.post('/', OrdenesController.crear);
+router.delete('/:id', OrdenesController.eliminar);
+router.put('/:id', OrdenesController.actualizar);
+router.get('/:id', OrdenesController.obtenerPorId);
+router.get('/cliente/:clienteId', OrdenesController.obtenerPorCliente);
+
+module.exports = router;
