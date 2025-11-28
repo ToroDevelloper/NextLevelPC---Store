504a04e7270bb34693c0951e34f70e1ece018d25
Tez-Tez-Tez <tezclaudia4@gmail.com>
2025-11-28 06:56:50 -0500

    iconos

--- fronted/src/components/Footer.jsx
+++ fronted/src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Footer.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Footer = () => {
   return (
@@ -26,19 +27,19 @@ const Footer = () => {
             <h4>Contacto</h4>
             <div className="contact-info">
               <div className="contact-item">
-                <span>✉️</span>
+                <i className="fas fa-envelope"></i>
                 <span>NextLevelPC@gmail.com</span>
               </div>
               <div className="contact-item">
-                <span>📞</span>
+                <i className="fas fa-phone"></i>
                 <span>+1 (555) 123-4567</span>
               </div>
               <div className="contact-item">
-                <span>🛠️</span>
+                <i className="fas fa-tools"></i>
                 <span>Soporte Técnico</span>
               </div>
               <div className="contact-item">
-                <span>🕒</span>
+                <i className="fas fa-clock"></i>
                 <span>Lun-Vie: 9:00-18:00</span>
               </div>
             </div>
@@ -49,25 +50,25 @@ const Footer = () => {
             <h4>Síguenos</h4>
             <div className="social-links">
               <a href="#" className="social-link" aria-label="Instagram">
-                <span>📸</span>
+                <i className="fab fa-instagram"></i>
               </a>
               <a href="#" className="social-link" aria-label="Facebook">
-                <span>👥</span>
+                <i className="fab fa-facebook"></i>
               </a>
               <a href="#" className="social-link" aria-label="YouTube">
-                <span>📺</span>
+                <i className="fab fa-youtube"></i>
               </a>
               <a href="#" className="social-link" aria-label="Twitter">
-                <span>🐦</span>
+                <i className="fab fa-twitter"></i>
               </a>
             </div>
             <div className="contact-info">
               <div className="contact-item">
-                <span>📍</span>
+                <i className="fas fa-map-marker-alt"></i>
                 <span>Mocoa, Colombia</span>
               </div>
               <div className="contact-item">
-                <span>🚀</span>
+                <i className="fas fa-shipping-fast"></i>
                 <span>Envíos Nacionales</span>
               </div>
             </div>
